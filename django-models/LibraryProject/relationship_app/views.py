b87bfcd0c9931caa6c19ed06e73f3bfc61fe3d57
Lilprex-fun <kimangaprecious@gmail.com>
2025-12-15 17:23:45 +0300

    Made changes to pass checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ from .models import Book, Library, UserProfile  # make sure UserProfile is impor
 # ---------- BOOK & LIBRARY VIEWS ----------
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
