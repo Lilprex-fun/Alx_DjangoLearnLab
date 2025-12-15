78a07a2351fc0867c4b88518133a3bc02ba3c0c1
Lilprex-fun <kimangaprecious@gmail.com>
2025-12-15 18:44:03 +0300

    Made changes for checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import list_books      # Function-based view for listing all books
-from .views import LibraryDetailView  # Class-based view for a specific library
+from .views import LibraryDetailView
+from  .views import (
+    register, LoginView, LogoutView,
+    admin_view, librarian_view, member_view, add_book, edit_book, delete_book  
+)  # Class-based view for a specific library
 
 urlpatterns = [
     # Function-based view: List all books
@@ -8,4 +12,20 @@ urlpatterns = [
 
     # Class-based view: Show details for a specific library
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # ---------- ROLE-BASED VIEWS ----------
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+
+    # ---------- SECURED BOOK VIEWS ----------
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
+
+
